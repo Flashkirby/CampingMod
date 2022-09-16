f3fc4d1e8642013c51a8e4e11967092b47b5bec5
Flashkirby <flashkirby@gmail.com>
2022-09-16 15:23:45 +0100

    formatting

--- CampingMod.cs
+++ CampingMod.cs
@@ -6,28 +6,23 @@ using Terraria.UI;
 
 namespace CampingMod
 {
-    public class CampingMod : Mod
-    {
+    public class CampingMod : Mod {
         public const string LANG_KEY = "Mods.CampingMod.";
 
-        public CampingMod()
-        {
+        public CampingMod() {
             Sets.TemporarySpawn = new HashSet<int>();
         }
 
-        public override void Load()
-        {
+        public override void Load() {
             SpawnInterfaceHelper.Load(this);
         }
 
-        public override void Unload()
-        {
+        public override void Unload() {
             Sets.TemporarySpawn = null;
             SpawnInterfaceHelper.Unload();
         }
 
-        public static class Sets
-        {
+        public static class Sets {
             /// <summary> Tiles which are identified as temporary spawn points. </summary>
             public static HashSet<int> TemporarySpawn;
         }
@@ -36,8 +31,7 @@ namespace CampingMod
         /// Prints a value from the mod's language file
         /// </summary>
         /// <param name="text"></param>
-        public static void Print(string text, byte r = 255, byte g = 255, byte b = 255)
-        {
+        public static void Print(string text, byte r = 255, byte g = 255, byte b = 255) {
             Main.NewText(Language.GetTextValue(LANG_KEY + text), r, g, b);
         }
 
@@ -45,19 +39,15 @@ namespace CampingMod
         /// Prints a value from the mod's language file in Yellow
         /// </summary>
         /// <param name="text"></param>
-        public static void PrintInfo(string text)
-        {
+        public static void PrintInfo(string text) {
             Main.NewText(Language.GetTextValue(LANG_KEY + text), 255, 240, 20);
         }
     }
 
-    public class CampingSystem : ModSystem
-    {
-        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-        {
+    public class CampingSystem : ModSystem {
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) {
             int deathTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Death Text"));
-            if (deathTextIndex != -1)
-            {
+            if (deathTextIndex != -1) {
                 layers.Insert(deathTextIndex, new LegacyGameInterfaceLayer(
                     "CampingMod: Spawn Interface",
                     delegate {
