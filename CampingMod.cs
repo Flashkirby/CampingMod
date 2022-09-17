8a26188d480c15440c5fa3f6bf5c56b7477c79d3
Flashkirby <flashkirby@gmail.com>
2022-09-17 15:43:26 +0100

    Move camping system to namespace

--- CampingMod.cs
+++ CampingMod.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using Terraria;
 using Terraria.Localization;
 using Terraria.ModLoader;
-using Terraria.UI;
 
 namespace CampingMod
 {
@@ -41,22 +40,6 @@ namespace CampingMod
         /// <param name="text"></param>
         public static void PrintInfo(string text) {
             Main.NewText(Language.GetTextValue(LANG_KEY + text), 255, 240, 20);
-        }
-    }
-
-    public class CampingSystem : ModSystem {
-        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) {
-            int deathTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Death Text"));
-            if (deathTextIndex != -1) {
-                layers.Insert(deathTextIndex, new LegacyGameInterfaceLayer(
-                    "CampingMod: Spawn Interface",
-                    delegate {
-                        SpawnInterfaceHelper.DrawInterface(Main.spriteBatch);
-                        return true;
-                    },
-                    InterfaceScaleType.UI)
-                );
-            }
         }
     }
 }
---
+++ Common/Systems/CampingSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace CampingMod.Common.Systems
+{
+
+    public class CampingSystem : ModSystem
+    {
+        /// <summary>
+        /// Display button interface for choosing tent spawn vs home spawn
+        /// </summary>
+        /// <param name="layers"></param>
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) {
+            int deathTextIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Death Text"));
+            if (deathTextIndex != -1) {
+                layers.Insert(deathTextIndex, new LegacyGameInterfaceLayer(
+                    "CampingMod: Spawn Interface",
+                    delegate {
+                        SpawnInterfaceHelper.DrawInterface(Main.spriteBatch);
+                        return true;
+                    },
+                    InterfaceScaleType.UI)
+                );
+            }
+        }
+    }
+}
